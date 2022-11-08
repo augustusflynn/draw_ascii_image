5c41f058264d76e8f207919011ed1b8ad4b24b3f
augustustung <huytung139@gmail.com>
2022-11-08 16:52:38 +0700

    add animation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [arrText, setArrText] = useState([])
+
   useEffect(() => {
     fetch(window.location.href + "1.txt")
       .then(res => res.text())
       .then((text) => {
-        const containerEl = document.getElementById("picture");
-        let arrText = text.split("\n")
-        for (let i = 0; i < arrText.length; i++) {
-          const newPreId = "pre" + i
-          if (!document.getElementById(newPreId)) {
-            let preEl = document.createElement("pre")
-            preEl.id = "pre" + i
-            preEl.textContent = arrText[i];
-            preEl.style.setProperty('font-size', '12px')
-            containerEl.appendChild(preEl);
-          }
-        }
+        setArrText(text.split("\n").map((_text, index) => ({ text: _text, delay: index * 600 })))
       });
   }, [])
 
+  useEffect(() => {
+    function animate(i) {
+      let sectionEl = document.getElementById("fade-in-" + i)
+      sectionEl.classList.add("fade-in-text")
+      setTimeout(() => {
+        sectionEl.classList.remove("fade-in-text", "opacity-0")
+        sectionEl.classList.add("opacity-1")
+        animate(i + 1)
+      }, 500)
+    }
+    if (arrText.length > 0) {
+      animate(0)
+    }
+  }, [arrText])
+
   return (
     <div id="picture">
+      {
+        arrText.map((val, i) => {
+          return (
+            <section
+              key={"fade-in-" + i}
+              id={"fade-in-" + i}
+              className="opacity-0"
+            >
+              <pre
+                key={val.text}
+                style={{
+                  fontSize: 12
+                }}
+              >{val.text}</pre>
+            </section>
+          )
+        })
+      }
     </div>
   );
 }
